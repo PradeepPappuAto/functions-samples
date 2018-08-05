a12efc9bd82d24938c7c58ac89401d60790bdc6d
a1force.co@gmail.com <a1force.co@gmail.com>
2018-08-05 22:02:15 +1000

    paypal server changes

--- paypal/functions/index.js
+++ paypal/functions/index.js
@@ -12,29 +12,74 @@ paypal.configure({
   client_secret: functions.config().paypal.client_secret // run: firebase functions:config:set paypal.client_secret="yourPaypalClientSecret"
 });
 
-exports.process = functions.https.onRequest((req, res) => {
-  if(req.method !== "POST"){
-    res.status(400).send('Please send a POST request');
-    return;
-  }
-  let data = req.body;
-  let requestJson = JSON.parse(data)
-  const paymentId = requestJson.paymentId;
-  const payerId = {
-    payer_id: requestJson.payerId
-  };
-  return paypal.payment.execute(paymentId, payerId, (error, payment) => {
-    res.header('Content-Type', 'application/json')
+exports.pay = functions.https.onRequest((req, res) => {
+  // 1.Set up a payment information object, Build PayPal payment request
+  const payReq = JSON.stringify({
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal'
+    },
+    redirect_urls: {
+      return_url: `${req.protocol}://${req.get('host')}/process`,
+      cancel_url: `${req.protocol}://${req.get('host')}/cancel`
+    },
+    transactions: [{
+      amount: {
+        total: req.body.price,
+        currency: 'AUD'
+      },
+      reference_id: req.body.orderId,
+      invoice_number: req.body.orderId
+    }]
+  });
+  // 2.Initialize the payment and redirect the user.
+  paypal.payment.create(payReq, (error, payment) => {
+    const links = {};
     if (error) {
       console.error(error);
-      res.json("{\"successful\":false}")
+      res.status('500').end();
     } else {
-      if (payment.state === 'approved') {
-        res.json("{\"successful\":true}")
+      // Capture HATEOAS links
+      payment.links.forEach((linkObj) => {
+        links[linkObj.rel] = {
+          href: linkObj.href,
+          method: linkObj.method
+        };
+      });
+      // If redirect url present, redirect user
+      if (links.hasOwnProperty('approval_url')) {
+        // REDIRECT USER TO links['approval_url'].href
+        console.info(links.approval_url.href);
+        // res.json({"approval_url":links.approval_url.href});
+        res.redirect(302, links.approval_url.href);
       } else {
-        console.error("Not approved");
-        res.json("{\"successful\":false}")
+        console.error('no redirect URI present');
+        res.status('500').end();
       }
     }
-  }).then(r => console.info('promise: ', r));
+  });
+});
+
+exports.cancel = functions.https.onRequest((req, res) => {
+  res.status('500').end();
+});
+
+exports.process = functions.https.onRequest((req, res) => {
+  console.log("Processing now..")
+  const paymentId = req.query.paymentId;
+  const payerId = {
+    payer_id: req.query.PayerID
+  };
+  return paypal.payment.execute(paymentId, payerId, (error, payment) => {
+    if (error) {
+      console.error(error);
+      res.status('500').end();
+    } else {
+      if (payment.state === 'approved') {
+        res.status('200').send(paymentId)
+      } else {
+        res.status('500').end();
+      }
+    }
+  })
 });
